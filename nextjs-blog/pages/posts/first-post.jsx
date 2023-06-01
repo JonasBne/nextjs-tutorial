717bae15eb65aac3689cfb3ae3dd4287a82f4115
Jonas Boone <boone.jonas@outlook.be>
2023-06-01 16:15:17 +0200

    feat: assets, metadata and css

--- nextjs-blog/pages/posts/first-post.jsx
+++ nextjs-blog/pages/posts/first-post.jsx
@@ -1,12 +1,14 @@
+import Head from 'next/head';
 import Link from 'next/link';
+import Layout from '../../components/Layout';
 
 export default function FirstPost() {
   return (
-    <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
-    </>
+    <Layout>
+        <Head>
+            <title>First Post</title>
+        </Head>
+        <h1>First Post</h1>
+    </Layout>
   );
 }
